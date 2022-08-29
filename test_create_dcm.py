8e6f7b2a2b5f32cc29d605562665f3a8facc3220
Tom Roberts <t.roberts@kcl.ac.uk>
2022-08-29 15:35:55 +0100

    Update test_create_dcm.py

--- test_create_dcm.py
+++ test_create_dcm.py
@@ -1,4 +1,10 @@
 import nii2dcm.dcm
 
-TestDicom = nii2dcm.dcm.Dicom('testfile.dcm')
+TestDicom = nii2dcm.dcm.Dicom('testDicom.dcm')
 TestDicom.save_as()
+
+TestDicomMRI = nii2dcm.dcm.DicomMRI('testDicomMri.dcm')
+TestDicomMRI.save_as()
+
+TestDicomMRISVR = nii2dcm.dcm.DicomMRISVR('testDicomMriSVR.dcm')
+TestDicomMRISVR.save_as()

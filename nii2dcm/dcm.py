4e6745c19114582ebb3f0daac94420557463d07f
Tom Roberts <t.roberts@kcl.ac.uk>
2022-08-29 14:54:05 +0100

    Update Dicom class with many more basic tags

--- nii2dcm/dcm.py
+++ nii2dcm/dcm.py
@@ -5,8 +5,9 @@ Tom Roberts
 """
 
 import tempfile
-import pydicom
-from pydicom.dataset import Dataset, FileDataset, FileMetaDataset
+import datetime
+import os
+from pydicom.dataset import FileDataset, FileMetaDataset
 from pydicom.uid import UID
 from pydicom.datadict import DicomDictionary, keyword_dict
 from pydicom.sequence import Sequence
@@ -16,22 +17,117 @@ import nibabel as nib
 class Dicom:
     """
     Creates basic DICOM structure
+
+    Assumptions:
+    - is_implicit_VR = False
+    - is_little_endian = True
+
     """
 
     def __init__(self, filename=tempfile.NamedTemporaryFile(suffix='.dcm').name):
+
+        # TODO:
+        # - add in correct UIDs
+        # - arrange tags into Series/Instance tags
 
         self.filename = filename
 
         # Creates minimal FileMeta
         self.file_meta = FileMetaDataset()
+        self.file_meta.FileMetaInformationGroupLength = ''
+        self.file_meta.FileMetaInformationVersion = ''
+        # self.file_meta.FileMetaInformationVersion = b'\x00\x01'  # Think redundant
         self.file_meta.MediaStorageSOPClassUID = ''
         self.file_meta.MediaStorageSOPInstanceUID = ''
         self.file_meta.ImplementationClassUID = ''
+        self.file_meta.ImplementationVersionName = 'nii2dcm_DICOM'
 
         # Creates minimal DataSet
         self.ds = FileDataset(filename, {}, file_meta=self.file_meta, preamble=b"\0" * 128)
+        self.ds.is_implicit_VR = False
+        self.ds.is_little_endian = True
+        self.ds.SpecificCharacterSet = "ISO_IR 100"
+        self.ds.ImageType = ['DERIVED', 'SECONDARY']
+        self.ds.ProtocolName = "nii2dcm_DICOM"
+
         self.ds.PatientName = "Test^Firstname"
         self.ds.PatientID = "12345678"
+        self.ds.AccessionNumber = "ABCXYZ"
+        self.ds.PatientBirthDate = ""
+        self.ds.PatientSex = ""
+        self.ds.PatientAge = ""
+        self.ds.PatientWeight = ""
+        self.ds.BodyPartExamined = ""
+
+        self.ds.InstitutionName = "INSTITUTION_NAME_NONE"
+        self.ds.Manufacturer = ""
+        self.ds.ManufacturerModelName = ""
+        self.ds.StudyDescription = ""
+        self.ds.InstanceCreatorUID = ''
+
+        self.ds.SOPClassUID = ''
+        self.ds.Modality = ''
+
+        dt = datetime.datetime.now()
+        dateStr = dt.strftime('%Y%m%d')
+        timeStr = dt.strftime('%H%M%S.%f')  # long format with micro seconds
+
+        self.ds.ContentDate = dateStr
+        self.ds.ContentTime = timeStr
+        self.ds.StudyDate = dateStr
+        self.ds.StudyTime = timeStr
+        self.ds.SeriesDate = dateStr
+        self.ds.SeriesTime = timeStr
+        self.ds.AcquisitionDate = dateStr
+        self.ds.AcquisitionTime = timeStr
+        self.ds.InstanceCreationDate = dateStr
+        self.ds.InstanceCreationTime = timeStr
+
+        self.ds.Rows = ""
+        self.ds.Columns = ""
+        self.ds.PixelSpacing = ""
+        self.ds.SamplesPerPixel = ""
+        self.ds.ImageOrientationPatient = ['1', '0', '0', '0', '1', '0']
+
+        self.ds.RescaleIntercept = ""
+        self.ds.RescaleSlope = ""
+        self.ds.WindowCenter = ""
+        self.ds.WindowWidth = ""
+
+        # per Instance tags
+        self.ds.SOPInstanceUID = ""
+        self.ds.InstanceNumber = ""
+        self.ds.SliceThickness = ""
+        self.ds.SpacingBetweenSlices = ""
+        self.ds.SliceLocation = ""
+        self.ds.ImagePositionPatient = ['0', '0', '0']
+
+        # per Series tags
+        self.ds.SeriesInstanceUID = ""
+        self.ds.SeriesNumber = ""
+        self.ds.AcquisitionNumber = ""
+
+        # # MRI DICOM tags
+        # # TODO: create MRI DICOM class
+        # self.ds.Modality = 'MR'
+        # self.file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.4'
+        # self.ds.SOPClassUID = '1.2.840.10008.5.1.4.1.1.4'
+        # self.ds.MRAcquisitionType = '' # '3D' for SVR
+
+        # # Omitted for now:
+        # 'FrameOfReferenceUID': uid_frame_of_reference,  # per SVR – think to do with localizer
+        # 'TemporalPositionIdentifier': '1',
+        # 'NumberOfTemporalPositions': '1',
+        # 'PositionReferenceIndicator': '',  # MC said v important, can be undefined
+        # 'InstitutionAddress': 'InstitutionAddress',
+        # 'ReferringPhysicianName': 'ReferringPhysicianName',
+        # 'CodeValue': 'CodeValue',
+        # 'CodingSchemeDesignator': 'CodingSchemeDesignator',
+        # 'CodeMeaning': 'CodeMeaning',
+        # 'StationName': 'StationName',
+        # 'InstitutionalDepartmentName': 'InstitutionalDepartmentName',
+        # 'PerformingPhysicianName': 'PerformingPhysicianName',
+        # 'OperatorsName': 'OperatorsName',
 
     def get_file_meta(self):
         return self.file_meta
@@ -48,8 +144,8 @@ class Dicom:
     #
     #     # fm - file_meta
     #     self.file_meta.FileMetaInformationVersion = b'\x00\x01'
-    #     self.file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.4'
-    #     self.file_meta.TransferSyntaxUID = '1.2.840.10008.1.2'
+    #
+    #
     #     self.file_meta.ImplementationClassUID = '1.2.276.0.7230010.3.0.3.6.1'
     #     self.file_meta.ImplementationVersionName = 'dicom_created_with_nii2dcm'
     #

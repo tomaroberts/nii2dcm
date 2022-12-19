f131de768762df542f546b73159d56b964989158
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-19 18:36:00 +0000

    Overhauls Nifti class – output single Dict containing nii hdr parameters

--- nii2dcm/nii.py
+++ nii2dcm/nii.py
@@ -9,16 +9,24 @@ import numpy as np
 
 class Nifti:
 
-    def get_nii2dcm_general_parameters(nib_nii):
+    def get_nii2dcm_parameters(nib_nii):
         """
-        Get general NIfTI file parameters relevant for DICOM creation.
+        Get general NIfTI header parameters relevant for DICOM tag transferal.
         :nib_nii - NIfTI loaded with nibabel
         :nii_parameters - parameters to transfer to DICOM header
         """
 
+        def fnT1N(A, N):
+            # Subfn: calculate T1N vector
+            # A = affine matrix [4x4]
+            # N = slice number (counting from 1)
+            T1N = A.dot([0, 0, N - 1, 1])
+            return T1N
+
+        # load nifti
         nii_img = nib_nii.get_fdata()
 
-        # Dimensions
+        # volume dimensions
         if nib_nii.header['dim'][4] == 1:
             nX, nY, nZ, nF = nib_nii.header['dim'][1], nib_nii.header['dim'][2], nib_nii.header['dim'][3], 1
             dimX, dimY, dimZ = nib_nii.header['pixdim'][1], nib_nii.header['pixdim'][2], nib_nii.header['pixdim'][3]
@@ -28,9 +36,7 @@ class Nifti:
 
         # Instances & Slice Spacing
         nInstances = nZ*nF
-
         sliceIndices = np.repeat(range(1, nZ+1), nF)
-
         voxelSpacing = dimZ
         zLocLast = (voxelSpacing * nZ) - voxelSpacing
         sliceLoca = np.repeat( np.linspace(0, zLocLast, num=nZ), nF)
@@ -48,14 +54,26 @@ class Nifti:
         fovY = nY * dimY
         fovZ = nZ * dimZ
 
-        nii2dcm_general_parameters = {
+        # slice positioning in 3-D space
+        # nb: -1 for dir cosines gives consistent orientation between Nifti and DICOM in ITK-Snap
+        A = nib_nii.affine
+        dircosX = -1 * A[:3, 0] / dimX
+        dircosY = -1 * A[:3, 1] / dimY
+
+        image_pos_patient_array = []
+        for iInstance in range(0, nInstances):
+            T1N = fnT1N(A, iInstance)
+            image_pos_patient_array.append([T1N[0], T1N[1], T1N[2]])
+
+        # output dictionary
+        nii2dcm_parameters = {
+
+            # series parameters
             'dimX': dimX,
             'dimY': dimY,
             'SliceThickness': str(dimZ),
             'SpacingBetweenSlices': str(dimZ),
             'AcquisitionMatrix': [0, nX, nY, 0],
-            'InstanceNumber': sliceIndices,
-            'SliceLocation': sliceLoca,
             'Rows': nX,
             'Columns': nY,
             'NumberOfSlices': nZ,
@@ -65,60 +83,16 @@ class Nifti:
             'WindowWidth': str(windowWidth),
             'RescaleIntercept': str(rescaleIntercept),
             'RescaleSlope': str(rescaleSlope),
+            'SpacingBetweenSlices': round(float(dimZ), 2),
+            'ImageOrientationPatient': [dircosY[0], dircosY[1], dircosY[2], dircosX[0], dircosX[1], dircosX[2]],
+            # alternative:
+            # 'ImageOrientationPatient': [dircosX[0], dircosX[1], dircosX[2], dircosY[0], dircosY[1], dircosY[2]],
+
+            # instance parameters
+            'InstanceNumber': sliceIndices,
+            'SliceLocation': sliceLoca,
+            'ImagePositionPatient': image_pos_patient_array
+
         }
 
-        return nii2dcm_general_parameters
-
-    def get_nii2dcm_geometry_parameters(nib_nii):
-        """
-        Create DICOM geometry tags from NIfTI affine
-        :nib_nii - NIfTI loaded with nibabel
-        :sliceIndex - index of slice within Nifti volume
-        """
-
-        def convert_nii2dcm_slice_parameters(nib_nii, sliceIndex):
-            """
-            Convert Nifti geometry parameters to DICOM geometry format
-            :nib_nii - NIfTI loaded with nibabel
-            :sliceIndex - index of slice within Nifti volume
-            """
-
-            def fnT1N(A, N):
-                # Subfn: calculate T1N vector
-                # A = affine matrix [4x4]
-                # N = slice number (counting from 1)
-                T1N = A.dot([[0], [0], [N - 1], [1]])
-                return T1N
-
-            dimX = nib_nii.header['pixdim'][1]
-            dimY = nib_nii.header['pixdim'][2]
-            dimZ = nib_nii.header['pixdim'][3]
-            dimF = nib_nii.header['pixdim'][4]
-
-            # Direction Cosines & Position Parameters
-            # nb: -1 for dir cosines gives consistent orientation between Nifti and DICOM in ITK-Snap
-            A = nib_nii.affine
-            dircosX = -1 * A[:3, 0] / dimX
-            dircosY = -1 * A[:3, 1] / dimY
-            T1N = fnT1N(A, sliceIndex)
-
-            dcm_geo_parameters = {
-                'SpacingBetweenSlices': round(float(dimZ), 2),
-                'ImagePositionPatient': [T1N[0], T1N[1], T1N[2]],
-
-                # nb: consistent orientation between Nifti and DICOM in ITK-Snap
-                'ImageOrientationPatient': [dircosY[0], dircosY[1], dircosY[2], dircosX[0], dircosX[1], dircosX[2]]
-
-                # Alternative:
-                # 'ImageOrientationPatient': [dircosX[0], dircosX[1], dircosX[2], dircosY[0], dircosY[1], dircosY[2]]
-            }
-
-            return dcm_geo_parameters
-
-        nInstances = nib_nii.header['dim'][3] * nib_nii.header['dim'][4]
-
-        nii2dcm_geometry_parameters = []
-        for iInstance in range(0, nInstances):
-            nii2dcm_geometry_parameters = convert_nii2dcm_slice_parameters(nib_nii, iInstance)
-
-        return nii2dcm_geometry_parameters
+        return nii2dcm_parameters

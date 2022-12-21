c00599719f7b3242838a4f88c37424fd6668624a
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-21 12:28:05 +0000

    Update create_3d_dicom.py

--- create_3d_dicom.py
+++ create_3d_dicom.py
@@ -1,33 +1,42 @@
 """
-Create 3D, single-frame DICOM dataset from NIfTI file
+Create 3D Fetal Brain MRI SVRTK single-frame DICOM dataset from NIfTI file
 """
+import os
 import nibabel as nib
-import nii2dcm.nii, nii2dcm.svr, nii2dcm.dcm_writer
+
+import nii2dcm.dcm_writer
+import nii2dcm.nii
+import nii2dcm.svr
 
 
 # Load Nifti file
-niiPath = r'/Users/tr17/data/DicomRecon/previous-recon/SVR-output.nii.gz'
-nii = nib.load(niiPath)
+niiInPath = r'/Users/tr17/data/DicomRecon/previous-recon/SVR-output.nii.gz'
+nii = nib.load(niiInPath)
 
-# Get Nifti parameters for transferal to DICOM
+# Set output directory
+dcmOutPath = r'/Users/tr17/code/nii2dcm/output'
+if os.path.exists(dcmOutPath):
+    if not os.path.isdir(dcmOutPath):
+        raise ValueError('The DICOM output path must be a directory.')
+else:
+    os.makedirs(dcmOutPath)
+
+# Get NIfTI parameters to transfer to DICOM
 nii2dcm_parameters = nii2dcm.nii.Nifti.get_nii2dcm_parameters(nii)
-# print(f'nii_parameters:\n {nii2dcm_parameters}')
 
 
 # Write single DICOM
 
 # Initialise
 TestDicomMRISVR = nii2dcm.svr.DicomMRISVR('testDicomMriSVR.dcm')
-# print(TestDicomMRISVR.ds)
 
 # Transfer Series tags
 nii2dcm.dcm_writer.transfer_nii_hdr_series_tags(TestDicomMRISVR, nii2dcm_parameters)
 
-# Get Nifti pixel data
+# Get NIfTI pixel data
 # TODO: create method in Nifti class – need to think about -1 value treatment
 nii_img = nii.get_fdata()
-iBkrd = nii_img == -1  # set background pixels = 0 (-1 in SVRTK)
-nii_img[iBkrd] = 0
+nii_img[nii_img == -1] = 0  # set background pixels = 0 (-1 in SVRTK)
 nii_img = nii_img.astype("uint16")  # match DICOM datatype
 
 # Set custom tags
@@ -38,12 +47,12 @@ TestDicomMRISVR.file_meta.TransferSyntaxUID = '1.2.840.10008.1.2.1'
 TestDicomMRISVR.ds.BitsAllocated = 16
 
 # Write DICOM Series, instance-by-instance
-num_slices = nii.shape[2]
-for slice_number in range(1, num_slices):
+for instance_index in range(0, nii2dcm_parameters['NumberOfInstances']):
 
     # Transfer Instance tags
-    nii2dcm.dcm_writer.transfer_nii_hdr_instance_tags(TestDicomMRISVR, nii2dcm_parameters, slice_number)
+    nii2dcm.dcm_writer.transfer_nii_hdr_instance_tags(TestDicomMRISVR, nii2dcm_parameters, instance_index)
 
     # Write slice
-    nii2dcm.dcm_writer.write_slice(TestDicomMRISVR, nii_img, slice_number, '')
-    # print(TestDicomMRISVR.ds)
+    nii2dcm.dcm_writer.write_slice(TestDicomMRISVR, nii_img, instance_index, dcmOutPath)
+
+print(TestDicomMRISVR.ds)

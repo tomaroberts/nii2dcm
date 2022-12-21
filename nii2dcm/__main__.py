87dbd9a99311587e172bb8d6b172ab31c330a1df
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-21 16:59:14 +0000

    Update __main__.py with argparse setup / add .toml

--- nii2dcm/__main__.py
+++ nii2dcm/__main__.py
@@ -4,31 +4,37 @@ import argparse
 from pathlib import Path
 
 
-def run(args=None):
+
+def cli(args=None):
     """
     Run nii2dcm via command line
     """
     if args is None:
         args = sys.argv[1:]
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="nii2dcm",
+        description="nii2dcm - NIfTI file to DICOM conversion"
+    )
 
-    parser.add_argument("input_path")
-    parser.add_argument("output_path")
+    parser.add_argument("input_file", type=str, help="[.nii/.nii.gz] input NIfTI file")
+    parser.add_argument("output_dir", type=str, help="[directory] output DICOM path")
+    parser.add_argument("-d", "--dicom_type", type=str, help="type of DICOM. e.g. MR, CT, US, XR, etc.")
+    parser.add_argument("-v", "--version", action="version", version="0.1.0")
 
     args = parser.parse_args()
 
-    input_path = Path(args.input_path)
-    output_path = Path(args.output_path)
+    input_file = Path(args.input_file)  # TODO: add check that file is .nii/.nii.gz
+    output_dir = Path(args.output_dir)
 
-    if not input_path.exists():
-        print(f"The input directory '{input_path}' does not exist")
+    if not input_file.exists():
+        print(f"The input file '{input_file}' does not exist")
         raise SystemExit(1)
 
-    if not output_path.exists():
-        print(f"The output directory '{output_path}' does not exist")
+    if not output_dir.exists():
+        print(f"The output directory '{output_dir}' does not exist")
         raise SystemExit(1)
 
 
 if __name__ == "__main__":
-    sys.exit(run())
+    sys.exit(cli())
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=["nii2dcm"],
     entry_points={
         "console_scripts": [
-            "nii2dcm = nii2dcm.__main__:run"
+            "nii2dcm = nii2dcm.__main__:cli"
         ]
     },
 )

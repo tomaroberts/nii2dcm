55a6c89d16bad4acfb5e8127a2ecf12dd62ae766
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-30 15:29:05 +0000

    Move BitsAllocated tag creation to DicomMRISVR class

--- scripts/create_3d_dicom.py
+++ scripts/create_3d_dicom.py
@@ -51,11 +51,7 @@ nii_img[nii_img < 0] = 0  # set background pixels = 0 (negative in SVRTK)
 nii_img = nii_img.astype("uint16")  # match DICOM datatype
 
 # Set custom tags
-# TODO: lines below for overriding and testing DICOM creation.
-#  Need to decide how best to implement properly, e.g.: force TransferSyntaxUID depending on Dicom subclass,
-#  or give user option to edit. Think former, with possibility of latter.
-TestDicomMRISVR.file_meta.TransferSyntaxUID = '1.2.840.10008.1.2.1'
-TestDicomMRISVR.ds.BitsAllocated = 16
+# - currently none
 
 # Write DICOM Series, instance-by-instance
 for instance_index in range(0, nii2dcm_parameters['NumberOfInstances']):

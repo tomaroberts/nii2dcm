b9fe72d407835e084f753b37bbb2a49882bdca9d
Tom Roberts <t.roberts@kcl.ac.uk>
2023-08-10 15:08:36 +0100

    Update CLI function with --ref_dicom option

--- nii2dcm/__main__.py
+++ nii2dcm/__main__.py
@@ -22,24 +22,35 @@ def cli(args=None):
 
     parser.add_argument("input_file", type=str, help="[.nii/.nii.gz] input NIfTI file")
     parser.add_argument("output_dir", type=str, help="[directory] output DICOM path")
-    parser.add_argument("-d", "--dicom_type", type=str, help="type of DICOM. e.g. MR, CT, US, XR, etc.")
+    parser.add_argument("-d", "--dicom_type", type=str, help="[string] type of DICOM. e.g. MR, CT, US, XR, etc.")
+    parser.add_argument("-r", "--ref_dicom", type=str, help="[.dcm] Reference DICOM file for Attribute transfer")
     parser.add_argument("-v", "--version", action="version", version="0.1.0")
 
     args = parser.parse_args()
 
     input_file = Path(args.input_file)  # TODO: add check that file is .nii/.nii.gz
-    output_dir = Path(args.output_dir)
+    output_dir = Path(args.output_dir)  # TODO: add check that this is directory
+    ref_dicom_file = Path(args.ref_dicom)   # TODO: add check that file is DICOM
 
     if not input_file.exists():
-        print(f"The input file '{input_file}' does not exist")
+        print(f"Input file '{input_file}' not found")
         raise SystemExit(1)
 
     if not output_dir.exists():
-        print(f"The output directory '{output_dir}' does not exist")
+        print(f"Output directory '{output_dir}' does not exist")
+        raise SystemExit(1)
+
+    if not ref_dicom_file.exists():
+        print(f"Reference DICOM file '{ref_dicom}' not found.")
         raise SystemExit(1)
 
     # execute nii2dcm
-    run_nii2dcm(input_file, output_dir, args.dicom_type)
+    run_nii2dcm(
+        input_file,
+        output_dir,
+        args.dicom_type,
+        ref_dicom_file
+    )
 
 
 if __name__ == "__main__":

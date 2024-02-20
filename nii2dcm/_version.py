a90cbc3fd55bf7ba18c2295c6a307904e474ea38
Tom Roberts <t.roberts@kcl.ac.uk>
2024-02-20 11:29:27 +0000

    Removes Style.SemVer call during serialize

--- nii2dcm/_version.py
+++ nii2dcm/_version.py
@@ -1,5 +1,3 @@
 import dunamai as _dunamai
 from dunamai import Version, Style
-__version__ = _dunamai.get_version(
-    "nii2dcm", third_choice=_dunamai.Version.from_any_vcs
-).serialize(metadata=False, style=Style.SemVer)
+__version__ = _dunamai.get_version("nii2dcm", third_choice=_dunamai.Version.from_any_vcs).serialize()

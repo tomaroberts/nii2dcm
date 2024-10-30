81d45adb8096430459d0d412c6b7d47e8737d837
Guillaume Theaud <guillaume.theaud@usherbrooke.ca>
2024-10-30 13:38:52 -0400

    Update version

--- nii2dcm/version.py
+++ nii2dcm/version.py
@@ -6,7 +6,7 @@ import glob
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 1
-_version_micro = 6  # use '' for first of series, number for 1 and above
+_version_micro = 0  # use '' for first of series, number for 1 and above
 _version_extra = ""  # Uncomment this for full releases
 
 # Construct full version string from these.
